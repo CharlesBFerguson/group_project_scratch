99f5d6a74df71e9c8d81c665b064407c0ba196f4
CharlesBFerguson <fergurburger326@gmail.com>
2020-03-31 14:03:26 -0400

    minor change

--- admin_02.js
+++ admin_02.js
@@ -9,13 +9,13 @@ app.use(bodyparser.json());
 
 // Username:   jgy4kfpqhzeplmwl     Password:   nal168vbk5cgeq50
 // Port:  3306    Database:   b0j93g47mct78nva
-host : 'mysql://jgy4kfpqhzeplmwl:nal168vbk5cgeq50@mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/b0j93g47mct78nva',
 var mysqlConnection = mysql.createConnection({
-      host : 'localhost',
+  // host : 'localhost',
+      host : 'mysql://jgy4kfpqhzeplmwl:nal168vbk5cgeq50@mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/b0j93g47mct78nva',
       user : 'jgy4kfpqhzeplmwl',
   password : 'nal168vbk5cgeq50',
   database : 'b0j93g47mct78nva'
-});
+})
 
 mysqlConnection.connect((err) => {
   if (!err)
